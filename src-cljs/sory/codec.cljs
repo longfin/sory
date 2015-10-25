54ec4eea091bf66a41c368dd8ca91f721c69ea68
longfin <longfinfunnel@gmail.com>
2015-10-25 16:26:47 +0900

    apply dual offset to durability

--- src-cljs/sory/codec.cljs
+++ src-cljs/sory/codec.cljs
@@ -3,34 +3,62 @@
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
 
-(defn normalize-freq [freq]
-  (if (> freq 18500)
-    (let [ifreq (js/Math.round freq)
-          m (mod ifreq 50)]
-      (/ (- (if (< m 25)
-              (- ifreq m)
-              (+ ifreq (- 50 m)))
-            19000)
-         50))))
+(def low-bits-offset 18500)
+(def high-bits-offset 19500)
+
+
+(defn- normalize-freq [freq]
+  (.debug js/console (str "pre-normalized: " freq))
+  (let [ifreq (js/Math.round freq)
+        m (mod ifreq 50)]
+    (if (< m 25)
+      (- ifreq m)
+      (+ ifreq (- 50 m)))))
+
+
+(defn- encode-bits [bs offset]
+  (.debug js/console (str "encoded: " (-> bs (* 50) (+ offset))))
+  (-> bs (* 50) (+ offset)))
+
+
+(defn- decode-bits [freq offset]
+  (-> freq (- offset) (/ 50)))
+
+
+(defn- validate-and-decode-bits [freq high-bits?]
+  (if high-bits?
+    (when (>= freq high-bits-offset)
+      (decode-bits freq high-bits-offset))
+    (when (and (<= low-bits-offset freq)
+               (<= freq high-bits-offset))
+      (decode-bits freq low-bits-offset))))
+
 
 (defn encode [s]
   (let [bs (map #(.charCodeAt %) s)]
-    (map
-     #(+ 19000 (* 50 %))
-     (interleave
-      (map #(bit-shift-right (bit-and % 0xF0) 4) bs)
-      (map #(bit-and % 0x0F) bs)))))
+    (interleave
+     (map #(-> %
+               (bit-and 0xF0)
+               (bit-shift-right 4)
+               (encode-bits high-bits-offset))
+          bs)
+     (map #(-> %
+               (bit-and 0x0F)
+               (encode-bits low-bits-offset))
+          bs))))
 
 
 (defn <decode [channel]
   (let [c (chan)]
     (go-loop [prev nil]
-      (when-let [freq (normalize-freq (<! channel))]
-        (if (nil? prev)
-          (recur freq)
-          (let [decoded (js/String.fromCharCode
-                         (bit-or (bit-shift-left prev 4)
-                                 freq))]
-            (put! c decoded)
-            (recur nil)))))
+      (.debug js/console (str "prev:" prev))
+      (let [freq (normalize-freq (<! channel))]
+        (when-let [bits (validate-and-decode-bits freq (nil? prev))]
+          (if (nil? prev)
+            (recur bits)
+            (let [prev-bits (bit-shift-left prev 4)
+                  decoded (js/String.fromCharCode
+                           (bit-or prev-bits bits))]
+              (put! c decoded))))
+        (recur nil)))
     c))
